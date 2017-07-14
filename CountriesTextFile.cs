cb51b9a9c9254745e305e56539fa47da24e3780d
Jillane Cook <cookjillane@gmail.com>
2017-07-13 22:42:57 -0400

    initial commit

---
+++ CountriesTextFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class CountriesTextFile
+    {
+        public void ListCountries()
+        {
+            Console.WriteLine("Please select an option: (1), (2), or (3)");
+            string input = (Console.ReadLine());
+
+            string filePath = @"C:\Users\cookj\Desktop\Countries.txt";
+
+
+
+            StreamReader reader = new StreamReader(filePath);
+
+            if (input == "1")
+            {
+
+                string line = reader.ReadToEnd();
+                input = reader.ReadToEnd();
+                Console.WriteLine(input);
+                Console.WriteLine(line);
+            } // do I need an exception here?
+
+        }
+
+        //public void WriteCountries()
+        //{
+        //    Console.WriteLine("Please select an option: (1), (2), or (3)");
+        //    string input = (Console.ReadLine());
+
+        //    StreamWriter writer;
+
+        //    string filePath = @"C:\Users\cookj\Desktop\Countries.txt";
+
+        //    if (input == "2")
+        //    {
+        //        writer = new StreamWriter(filePath);
+        //        writer.Write("ITALY ");
+        //        writer.WriteLine("SPAIN");
+        //        writer.WriteLine("ENGLAND");
+        //        writer.Close();
+
+        //    }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+    namespace FileIO
+        {
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CountriesTextFile list = new CountriesTextFile();
+            list.ListCountries();
+
+            //CountriesTextFile write = new CountriesTextFile();
+            //write.WriteCountries();    //when I run this code, the write countries overwrites the list of countries 
+            //from List Countries
+
+
+
+
+        }
+    }
+        
+}
